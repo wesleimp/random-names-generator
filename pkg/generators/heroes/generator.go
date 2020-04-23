e7c7467ce7ed5343f7a35141563038682bb15709
Weslei Juan Novaes Pereira <wesleimsr@gmail.com>
2020-04-23 18:58:15 -0300

    Removed preverb from heroe

--- pkg/generators/heroes/generator.go
+++ pkg/generators/heroes/generator.go
@@ -1,17 +1,14 @@
 package heroes
 
 import (
-	"fmt"
 	"strings"
 
-	"github.com/wesleimp/random-names-generator/pkg/generators/preverbs"
 	"github.com/wesleimp/random-names-generator/pkg/randomizer"
 )
 
 // Generate a new heroe
 func Generate() string {
-	pre := preverbs.GetRandom()
 	heroe := randomizer.GetRandom(heroes[:])
 
-	return strings.Title(fmt.Sprintf("%s %s", pre, heroe))
+	return strings.Title(heroe)
 }
